b17334300db795099fa8ee26df33ede00bd95cf9
Fuzailk2786 <123993330+Fuzailk2786@users.noreply.github.com>
2026-02-23 00:43:22 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,22 +1,22 @@
-const CACHE_NAME = 'islamic-quiz-v1';
+const CACHE_NAME = 'islamic-quest-v1';
 const assets = [
-  '/',
-  '/index.html',
-  '/manifest.json'
+  './',
+  './index.html',
+  'https://fonts.googleapis.com/css2?family=Cinzel:wght@700&family=Poppins:wght@300;400;600&display=swap'
 ];
 
-self.addEventListener('install', e => {
+self.addEventListener('install', (e) => {
   e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
+    caches.open(CACHE_NAME).then((cache) => {
       return cache.addAll(assets);
     })
   );
 });
 
-self.addEventListener('fetch', e => {
+self.addEventListener('fetch', (e) => {
   e.respondWith(
-    caches.match(e.request).then(response => {
-      return response || fetch(e.request);
+    caches.match(e.request).then((res) => {
+      return res || fetch(e.request);
     })
   );
 });
